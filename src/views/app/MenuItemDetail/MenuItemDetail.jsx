f1671632ff004696cc77d0e1816645b8fae131db
Duy Tran <txd22081999@gmail.com>
2021-06-27 18:37:37 +0700

    styling

--- src/views/app/MenuItemDetail/MenuItemDetail.jsx
+++ src/views/app/MenuItemDetail/MenuItemDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { Row, Col, Card, CardBody, CardTitle } from 'reactstrap'
+import { Row, Col, Card, CardBody, CardTitle, Button } from 'reactstrap'
 import axios from 'axios'
 
 import { getMenuItems } from '../../../redux/actions'
@@ -40,13 +40,15 @@ const MenuItemDetail = (props) => {
     setMenuItem(foundMenuItem)
   }, [menuItems])
 
-  // if (menuItems.length === 0 || !menuItem.id ) {
-  //   return <div className='loading'></div>
-  // }
+  if (menuItems.length === 0 || !menuItem.id) {
+    return <div className='loading'></div>
+  }
 
   return (
     <div className='ProductDetail'>
-      <p className='title'>Bún Thịt nướng</p>
+      <p className='title'>
+        {menuItem.name} <i className></i>{' '}
+      </p>
       <Separator className='mb-4 mt-3' />
       <Row>
         <Colxx md='8'>
@@ -54,8 +56,9 @@ const MenuItemDetail = (props) => {
             <CardBody>
               <div className='product-img'>
                 <img
-                  src='https://firebasestorage.googleapis.com/v0/b/merchant-dashboard-5c894.appspot.com/o/images%2Fcach-lam-bun-thit-nuong-tai-nha-don-gian-dam-da-thom-ngon-nuc-mui-2-760x367.jpeg?alt=media&token=1262a330-c423-4de7-a8f6-c8cdc6c2c794'
-                  alt=''
+                  // src='https://firebasestorage.googleapis.com/v0/b/merchant-dashboard-5c894.appspot.com/o/images%2Fcach-lam-bun-thit-nuong-tai-nha-don-gian-dam-da-thom-ngon-nuc-mui-2-760x367.jpeg?alt=media&token=1262a330-c423-4de7-a8f6-c8cdc6c2c794'
+                  src={menuItem.imageUrl}
+                  alt='product-cover-img'
                 />
               </div>
               {/* <div className='content'>{JSON.stringify(menuItem)}</div> */}
@@ -71,24 +74,36 @@ const MenuItemDetail = (props) => {
             <CardBody>
               <div className='section'>
                 <p className='section-heading'>Mô tả</p>
-                <p>Bún thịt nướng thập cẩm gồm: thịt, chả, nem, rau sống</p>
+                <p>{menuItem.description}</p>
               </div>
 
               <div className='section'>
                 <p className='section-heading'>
-                  Giá: <span className='text-orange'>45000đ</span>
+                  Giá: <span className='text-orange'>{menuItem.price}đ</span>
                 </p>
-              </div>
-
-              <div className='section'>
-                <p className='section-heading'>Đang kích hoạt</p>
               </div>
 
               <div className='section'>
                 <p className='section-heading'>
-                  Trạng thái: <span className='font-weight-500'>Còn hàng</span>
+                  {menuItem.isActive ? 'Đang kích hoạt' : 'Đang ẩn'}
                 </p>
               </div>
+
+              <div className='section'>
+                <p className='section-heading'>
+                  Trạng thái:{' '}
+                  <span className='font-weight-500'>
+                    {menuItem.state === 'IN_STOCK' ? 'Còn hàng' : 'Hết hàng'}
+                  </span>
+                </p>
+              </div>
+
+              <Button
+                color='primary'
+                style={{ marginLeft: 'auto', display: 'block' }}
+              >
+                Chỉnh sửa
+              </Button>
             </CardBody>
           </Card>
         </Colxx>
